0d11834632408c1838ffcb7b42c516f84b46fa65
judigot <judigot@github.com>
2024-01-16 17:29:08 +0800

    feat(fe): render snippets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,41 @@
 'use client';
 
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import {snippet} from '@prisma/client';
 
 export default function Home() {
-  const [count, setCount] = useState(0);
+  const [snippets, setSnippets] = useState<snippet[]>([]);
 
-  return (
-    <div style={{zoom: '500%', textAlign: 'center'}}>
-      <h1>Big Bang</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </div>
-  );
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/v1/snippet`, {
+      // *GET, POST, PATCH, PUT, DELETE
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      // For POST, PATCH, and PUT requests
+      // body: JSON.stringify({ key: "value" }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        // Success
+        setSnippets(result);
+      })
+      .catch((error) => {
+        // Failure
+        throw new Error(error);
+      });
+  }, []);
+
+  return snippets && <p>{JSON.stringify(snippets, null, 4)}</p>;
 }
+
+// import {PrismaClient, snippet} from '@prisma/client';
+// import DatatypeParser from '@/utils/DataTypeParser';
+
+// export default async function Home() {
+//   const prisma = new PrismaClient();
+//   let result = DatatypeParser(await prisma.snippet.findMany());
+//   return <p>{JSON.stringify(result, null, 4)}</p>;
+// }
