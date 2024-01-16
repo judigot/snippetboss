028b129d93c38ea4c30ec4d3f29fa77464773139
judigot <igot.judefrancis@seedtech.ph>
2024-01-16 23:20:08 +0800

    feat(db): add seed

--- src/prisma/seed/seed.ts
+++ src/prisma/seed/seed.ts
@@ -2,39 +2,41 @@ import {PrismaClient} from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-// prettier-ignore
 async function main() {
-  // await prisma.snippet_type.createMany({
-  //   data: [
-  //     { snippet_type_name: "global" },
-  //     { snippet_type_name: "specific" },
-  //   ],
-  // });
+  await prisma.snippet_type.createMany({
+    data: [{snippet_type_name: 'global'}, {snippet_type_name: 'specific'}],
+  });
 
-  // await prisma.language.createMany({
-  //   data: [
-  //     { lang_name: "typescript" },
-  //     { lang_name: "java" },
-  //   ],
-  // });
+  await prisma.language.createMany({
+    data: [{lang_name: 'typescript'}, {lang_name: 'java'}],
+  });
 
-  // await prisma.snippet.createMany({
-  //   data: [
-  //     {
-  //       lang_id: 1,
-  //       snippet_type_id: 1,
-  //       snippet_name: "log",
-  //       snippet_content: "console.log(${1:\"${2:Hello, World!}\"});",
-  //     },
-  //     {
-  //       lang_id: 1,
-  //       snippet_type_id: 1,
-  //       snippet_name: "logRed",
-  //       snippet_content: "console.log(`%c\\${${1:\"${2:Hello, World!}\"}}`, \"color: red\")",
-  //     }
-  //   ],
-  // });
-  
+  await prisma.prefix.createMany({
+    data: [{prefix_name: 'log'}, {prefix_name: 'logRed'}],
+  });
+
+  await prisma.snippet.createMany({
+    data: [
+      {
+        snippet_type_id: 1,
+        prefix_id: 1,
+        snippet_content: 'console.log(${1:"${2:Hello, World!}"});',
+      },
+      {
+        snippet_type_id: 1,
+        prefix_id: 2,
+        snippet_content:
+          'console.log(`%c\\${${1:"${2:Hello, World!}"}}`, "color: red")',
+      },
+    ],
+  });
+
+  await prisma.snippet_language.createMany({
+    data: [
+      {snippet_id: 1, language_id: 1},
+      {snippet_id: 2, language_id: 2},
+    ],
+  });
 }
 
 main()
