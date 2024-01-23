266453752a8cb2a83d1ca0715ae24d7b70783df1
judigot <igot.judefrancis@seedtech.ph>
2024-01-23 22:41:49 +0800

    chore(seed): refactor seed file

--- src/prisma/seed/seed.ts
+++ src/prisma/seed/seed.ts
@@ -3,51 +3,54 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.snippet_type.createMany({
-    data: [{ snippet_type_name: 'global' }, { snippet_type_name: 'specific' }],
-  });
+  try {
+    await prisma.snippet_type.createMany({
+      data: [
+        { snippet_type_name: 'global' },
+        { snippet_type_name: 'specific' },
+      ],
+    });
 
-  await prisma.language.createMany({
-    data: [
-      { lang_name: 'typescript', display_name: 'TypeScript' },
-      { lang_name: 'java', display_name: 'Java' },
-    ],
-  });
+    await prisma.language.createMany({
+      data: [
+        { lang_name: 'typescript', display_name: 'TypeScript' },
+        { lang_name: 'java', display_name: 'Java' },
+      ],
+    });
 
-  await prisma.prefix.createMany({
-    data: [{ prefix_name: 'log' }, { prefix_name: 'logRed' }],
-  });
+    await prisma.prefix.createMany({
+      data: [{ prefix_name: 'log' }, { prefix_name: 'logRed' }],
+    });
 
-  await prisma.snippet.createMany({
-    data: [
-      {
-        snippet_type_id: 1,
-        prefix_id: 1,
-        snippet_content: 'console.log(${1:"${2:Hello, World!}"});',
-      },
-      {
-        snippet_type_id: 1,
-        prefix_id: 2,
-        snippet_content:
-          'console.log(`%c\\${${1:"${2:Hello, World!}"}}`, "color: red")',
-      },
-    ],
-  });
+    await prisma.snippet.createMany({
+      data: [
+        {
+          snippet_type_id: 1,
+          prefix_id: 1,
+          snippet_content: 'console.log(${1:"${2:Hello, World!}"});',
+        },
+        {
+          snippet_type_id: 1,
+          prefix_id: 2,
+          snippet_content:
+            'console.log(`%c\\${${1:"${2:Hello, World!}"}}`, "color: red")',
+        },
+      ],
+    });
 
-  await prisma.snippet_language.createMany({
-    data: [
-      { snippet_id: 1, language_id: 1 },
-      { snippet_id: 2, language_id: 2 },
-    ],
-  });
+    await prisma.snippet_language.createMany({
+      data: [
+        { snippet_id: 1, language_id: 1 },
+        { snippet_id: 2, language_id: 2 },
+      ],
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+  .then(() => {})
+  .catch(() => {});
