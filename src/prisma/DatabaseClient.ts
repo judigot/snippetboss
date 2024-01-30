56d9b00a7951e41dac17d9ca8f279abccbf9d72f
judigot <judigot@github.com>
2024-01-30 14:54:53 +0800

    feat: use singleton instance for prisma client

--- src/prisma/DatabaseClient.ts
+++ src/prisma/DatabaseClient.ts
@@ -1,3 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+// export const prisma = new PrismaClient();
+
+import { PrismaClient } from '@prisma/client';
+class Prisma {
+  private static instance: PrismaClient;
+
+  private constructor() {}
+
+  public static getInstance(): PrismaClient {
+    if (!Prisma.instance) {
+      Prisma.instance = new PrismaClient();
+    }
+    return Prisma.instance;
+  }
+}
+
+// Singleton instance
+export const prisma = Prisma.getInstance();
