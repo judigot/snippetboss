221f22b2b3f3fbe92b97635f3496eeb643ed8bcc
judigot <judigot@github.com>
2024-02-05 17:13:12 +0800

    chore: add ids to seed data

--- src/prisma/seed/seed.ts
+++ src/prisma/seed/seed.ts
@@ -6,33 +6,42 @@ async function main() {
   try {
     await prisma.snippet_type.createMany({
       data: [
-        { snippet_type_name: 'global' },
-        { snippet_type_name: 'specific' },
+        { snippet_type_id: 1, snippet_type_name: 'global' },
+        { snippet_type_id: 2, snippet_type_name: 'specific' },
       ],
     });
 
     await prisma.language.createMany({
       data: [
-        { language_name: 'typescript', display_name: 'TypeScript' },
-        { language_name: 'java', display_name: 'Java' },
+        {
+          language_id: 1,
+          language_name: 'typescript',
+          display_name: 'TypeScript',
+        },
+        { language_id: 2, language_name: 'java', display_name: 'Java' },
       ],
     });
 
     await prisma.prefix.createMany({
       data: [
         {
+          prefix_id: 1,
           prefix_description: 'String Variable',
         },
         {
+          prefix_id: 2,
           prefix_description: 'Integer Number Variable',
         },
         {
+          prefix_id: 3,
           prefix_description: 'Description',
         },
         {
+          prefix_id: 4,
           prefix_description: 'Description',
         },
         {
+          prefix_id: 5,
           prefix_description: 'Description',
         },
         // {
