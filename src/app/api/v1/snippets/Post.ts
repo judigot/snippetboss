57d5af0d22c055ff6229dfaf528fce0c42e65d97
judigot <judigot@github.com>
2024-02-12 17:02:11 +0800

    feat: add insert snippet and snippet language

--- src/app/api/v1/snippets/Post.ts
+++ src/app/api/v1/snippets/Post.ts
@@ -1,18 +1,40 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/prisma/DatabaseClient';
-import { snippet } from '@prisma/client';
+import { language, snippet } from '@prisma/client';
 import DatatypeParser from '@/utils/DataTypeParser';
 
-interface Body extends Omit<snippet, 'snippet_id'> {}
+interface Body {
+  snippet: Omit<snippet, 'snippet_id'>;
+  language: language;
+}
 
 interface Response extends snippet {}
 
 const PostHandler = async (req: NextRequest) => {
   try {
     const body = (await req.json()) as Body;
-    const result: Response = await prisma.snippet.create({
-      data: body,
+
+    const result: Response = await prisma.$transaction(async (transaction) => {
+      // Insert the snippet
+      const newSnippetResult: Response = DatatypeParser(
+        await transaction.snippet.create({
+          data: body.snippet,
+        }),
+      );
+
+      const newSnippetID = newSnippetResult.snippet_id;
+
+      // Insert snippet language to junction table
+      await transaction.snippet_language.create({
+        data: {
+          snippet_id: newSnippetID,
+          language_id: body.language.language_id,
+        },
+      });
+
+      return newSnippetResult;
     });
+
     return NextResponse.json(DatatypeParser(result), {
       status: 200,
     });
