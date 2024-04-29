3bf482c1580986a8586fe2ffcf8fa3f34168e3a3
judigot <judigot@gmail.com>
2024-04-29 14:59:38 +0800

    feat: fix DataTypeParser null or undefined error

--- src/app/api/v1/prefixes/Get.ts
+++ src/app/api/v1/prefixes/Get.ts
@@ -14,59 +14,59 @@ const GetHandler = async (req: NextRequest) => {
     const unusedByLanguage = req.nextUrl.searchParams.get('unused-by-language');
     if (unusedByLanguage !== null) {
       const sql: string = /*sql*/ `
-        WITH LanguageCheck AS (
-          SELECT 1
-          FROM "language"
-          WHERE "language_name" = '${unusedByLanguage}'
-        ),
-        PrefixesNotUsedByLanguage AS (
-          SELECT
-            p.*,
-            json_agg(pn.*) AS prefix_names
-          FROM "prefix" p
-          JOIN prefix_name pn ON p.prefix_id = pn.prefix_id
-          WHERE NOT EXISTS (
-            SELECT 1
-            FROM "snippet" s
-            JOIN "snippet_language" sl ON s."snippet_id" = sl."snippet_id"
-            JOIN "language" l ON sl."language_id" = l."language_id"
-            WHERE l."language_name" = '${unusedByLanguage}'
-            AND s."prefix_id" = p."prefix_id"
-          )
-          GROUP BY p.prefix_id
-        )
-        SELECT pnu.*
-        FROM PrefixesNotUsedByLanguage pnu
-        WHERE EXISTS (SELECT 1 FROM LanguageCheck);
-
         -- WITH LanguageCheck AS (
-        -- SELECT 1
-        -- FROM "language"
-        -- WHERE "language_name" = 'java'
+        --   SELECT 1
+        --   FROM "language"
+        --   WHERE "language_name" = '${unusedByLanguage}'
         -- ),
         -- PrefixesNotUsedByLanguage AS (
-        -- SELECT
-        -- p.*,
-        -- json_agg(pn.*) AS prefix_names
-        -- FROM "prefix" p
-        -- LEFT JOIN prefix_language pl ON p.prefix_id = pl.prefix_id
-        -- LEFT JOIN prefix_name pn ON p.prefix_id = pn.prefix_id
-        -- WHERE pl.prefix_id IS NULL -- Prefixes not in prefix_language
-        -- OR EXISTS (
-        -- SELECT 1
-        -- FROM "language" l
-        -- JOIN prefix_language pl2 ON l.language_id = pl2.language_id
-        -- WHERE l.language_name = 'java' AND p.prefix_id = pl2.prefix_id
+        --   SELECT
+        --     p.*,
+        --     json_agg(pn.*) AS prefix_names
+        --   FROM "prefix" p
+        --   JOIN prefix_name pn ON p.prefix_id = pn.prefix_id
+        --   WHERE NOT EXISTS (
+        --     SELECT 1
+        --     FROM "snippet" s
+        --     JOIN "snippet_language" sl ON s."snippet_id" = sl."snippet_id"
+        --     JOIN "language" l ON sl."language_id" = l."language_id"
+        --     WHERE l."language_name" = '${unusedByLanguage}'
+        --     AND s."prefix_id" = p."prefix_id"
+        --   )
+        --   GROUP BY p.prefix_id
         -- )
-        -- GROUP BY p.prefix_id
-        -- )
-        -- SELECT *
-        -- FROM PrefixesNotUsedByLanguage
-        -- WHERE EXISTS (
-        -- SELECT 1
-        -- FROM LanguageCheck);
+        -- SELECT pnu.*
+        -- FROM PrefixesNotUsedByLanguage pnu
+        -- WHERE EXISTS (SELECT 1 FROM LanguageCheck);
+
+        WITH LanguageCheck AS (
+        SELECT 1
+        FROM "language"
+        WHERE "language_name" = 'java'
+        ),
+        PrefixesNotUsedByLanguage AS (
+        SELECT
+        p.*,
+        json_agg(pn.*) AS prefix_names
+        FROM "prefix" p
+        LEFT JOIN prefix_language pl ON p.prefix_id = pl.prefix_id
+        LEFT JOIN prefix_name pn ON p.prefix_id = pn.prefix_id
+        WHERE pl.prefix_id IS NULL -- Prefixes not in prefix_language
+        OR EXISTS (
+        SELECT 1
+        FROM "language" l
+        JOIN prefix_language pl2 ON l.language_id = pl2.language_id
+        WHERE l.language_name = 'java' AND p.prefix_id = pl2.prefix_id
+        )
+        GROUP BY p.prefix_id
+        )
+        SELECT *
+        FROM PrefixesNotUsedByLanguage
+        WHERE EXISTS (
+        SELECT 1
+        FROM LanguageCheck);
       `;
-      console.log(sql);
+      // console.log(sql);
       const result: PrefixResponse =
         await prisma.$queryRaw`${Prisma.sql([sql])}`;
       return NextResponse.json(DatatypeParser(result));
--- src/utils/DataTypeParser.ts
+++ src/utils/DataTypeParser.ts
@@ -30,35 +30,38 @@ export default function DatatypeParser<T extends RowData | RowData[]>(
 const castRowValues = (data: RowData): RowData => {
   const rows: RowData = {};
 
-  for (const [key, value] of Object.entries(data)) {
-    switch (typeof value) {
-      case 'bigint':
-        rows[key] = Number(value);
-        break;
-      case 'object':
-        if (value instanceof Decimal) {
-          rows[key] = parseFloat(String(value));
-        }
+  if (data) {
+    for (const [key, value] of Object.entries(data)) {
+      switch (typeof value) {
+        case 'bigint':
+          rows[key] = Number(value);
+          break;
+        case 'object':
+          if (value instanceof Decimal) {
+            rows[key] = parseFloat(String(value));
+          }
 
-        // Array
-        if (
-          value instanceof Array &&
-          Array.isArray(value) &&
-          value.constructor.name === 'Array'
-        ) {
-          // Recurse if row is an array
-          rows[key] = DatatypeParser(value);
-        }
+          // Array
+          if (
+            value instanceof Array &&
+            Array.isArray(value) &&
+            value.constructor.name === 'Array'
+          ) {
+            // Recurse if row is an array
+            rows[key] = DatatypeParser(value);
+          }
 
-        // Date/Datetime
-        if (value instanceof Date) {
-          rows[key] = new Date(value);
-        }
-        break;
-      default:
-        rows[key] = value;
-        break;
+          // Date/Datetime
+          if (value instanceof Date) {
+            rows[key] = new Date(value);
+          }
+          break;
+        default:
+          rows[key] = value;
+          break;
+      }
     }
   }
+
   return rows;
 };
