45db7a16755d7c2e57de34eccc1e827e5125ce4f
judigot <judigot@gmail.com>
2024-04-29 17:08:43 +0800

    feat: exclude prefixes that are specific to another language

--- src/app/api/v1/prefixes/Get.ts
+++ src/app/api/v1/prefixes/Get.ts
@@ -14,57 +14,46 @@ const GetHandler = async (req: NextRequest) => {
     const unusedByLanguage = req.nextUrl.searchParams.get('unused-by-language');
     if (unusedByLanguage !== null) {
       const sql: string = /*sql*/ `
-        -- WITH LanguageCheck AS (
-        --   SELECT 1
-        --   FROM "language"
-        --   WHERE "language_name" = '${unusedByLanguage}'
-        -- ),
-        -- PrefixesNotUsedByLanguage AS (
-        --   SELECT
-        --     p.*,
-        --     json_agg(pn.*) AS prefix_names
-        --   FROM "prefix" p
-        --   JOIN prefix_name pn ON p.prefix_id = pn.prefix_id
-        --   WHERE NOT EXISTS (
-        --     SELECT 1
-        --     FROM "snippet" s
-        --     JOIN "snippet_language" sl ON s."snippet_id" = sl."snippet_id"
-        --     JOIN "language" l ON sl."language_id" = l."language_id"
-        --     WHERE l."language_name" = '${unusedByLanguage}'
-        --     AND s."prefix_id" = p."prefix_id"
-        --   )
-        --   GROUP BY p.prefix_id
-        -- )
-        -- SELECT pnu.*
-        -- FROM PrefixesNotUsedByLanguage pnu
-        -- WHERE EXISTS (SELECT 1 FROM LanguageCheck);
-
-        WITH LanguageCheck AS (
-        SELECT 1
-        FROM "language"
-        WHERE "language_name" = '${unusedByLanguage}'
-        ),
-        PrefixesNotUsedByLanguage AS (
-        SELECT
-        p.*,
+WITH UnusedPrefixes AS (
+    SELECT p.prefix_id,
+        p.prefix_description,
+        p.snippet_type_id,
         json_agg(pn.*) AS prefix_names
-        FROM "prefix" p
-        LEFT JOIN prefix_language pl ON p.prefix_id = pl.prefix_id
-        LEFT JOIN prefix_name pn ON p.prefix_id = pn.prefix_id
-        WHERE pl.prefix_id IS NULL -- Prefixes not in prefix_language
-        OR EXISTS (
-        SELECT 1
-        FROM "language" l
-        JOIN prefix_language pl2 ON l.language_id = pl2.language_id
-        WHERE l.language_name = '${unusedByLanguage}' AND p.prefix_id = pl2.prefix_id
+    FROM prefix p
+        JOIN prefix_name pn ON p.prefix_id = pn.prefix_id
+    WHERE NOT EXISTS (
+            SELECT 1
+            FROM snippet s
+                JOIN snippet_language sl ON s.snippet_id = sl.snippet_id
+                JOIN language l ON sl.language_id = l.language_id
+            WHERE l.language_name = '${unusedByLanguage}'
+                AND s.prefix_id = p.prefix_id
         )
-        GROUP BY p.prefix_id ORDER BY p.prefix_id
-        )
-        SELECT *
-        FROM PrefixesNotUsedByLanguage
-        WHERE EXISTS (
+    GROUP BY p.prefix_id,
+        p.prefix_description,
+        p.snippet_type_id
+),
+IncludeSpecificSnippets AS (
+    SELECT pl.prefix_id
+    FROM prefix_language pl
+        JOIN language l ON pl.language_id = l.language_id
+    WHERE l.language_name = '${unusedByLanguage}'
+)
+SELECT up.prefix_id,
+    up.prefix_description,
+    up.snippet_type_id,
+    up.prefix_names
+FROM UnusedPrefixes up
+WHERE up.prefix_id IN (
+        SELECT prefix_id
+        FROM IncludeSpecificSnippets
+    )
+    OR NOT EXISTS (
         SELECT 1
-        FROM LanguageCheck);
+        FROM prefix_language pl
+        WHERE pl.prefix_id = up.prefix_id
+    )
+ORDER BY up.prefix_id;
       `;
       console.log(sql);
       const result: PrefixResponse =
